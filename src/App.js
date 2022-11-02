cefedbafe057f2e348fe2d61637b6ad7e5f080ed
cakalmev <cakalmevlut11@gmail.com>
2022-11-02 16:09:43 +0100

    started

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.scss"
+
+
+const App = ()=>{
+  return (
+
+<div className="App">
+
+<h1>
+  Welcome React 
+</h1>
+
+<p> lorem ipsum dolor sit am
+
+</p>
+
+</div>
+
+
+
+
+  )
+
+
+
+
+};
+
+export default App;
